ac1b4ec559b7addb343a824f4623099ad725aa41
so0 <syngshm@gmail.com>
2020-10-04 16:13:26 +0900

    CounterContainer useSelector최적화

--- learn-redux/src/containers/CounterContainer.js
+++ learn-redux/src/containers/CounterContainer.js
@@ -1,16 +1,31 @@
 // 컨테이너 컴포넌트 : 리덕스 스토어의 상태를 조회하거나, 액션을 디스패치 할 수 있는 컴포넌트를 의미합니다. 다른 프리젠테이셔널 컴포넌트들을 불러와서 사용
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch, shallowEqual } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease, setDiff } from '../modules/counter';
 
 function CounterContainer() {
   // useSelector는 리덕스 스토어의 상태를 조회하는 Hook
   // state의 값은 store.getState() 함수를 호출했을 때 나타나는 결과물과 동일
-  const { number, diff } = useSelector((state) => ({
-    number: state.counter.number,
-    diff: state.counter.diff,
-  }));
+  // const { number, diff } = useSelector((state) => ({
+  //   number: state.counter.number,
+  //   diff: state.counter.diff,
+  // }));
+  //  useSelector Hook 을 통해 매번 렌더링 될 때마다 새로운 객체 { number, diff }를 만드는 것이기 때문에 상태가 바뀌었는지 바뀌지 않았는지 확인을 할 수 없어서 낭비 렌더링이 이루어지고 있는 것
+
+  /* useSelector 최적화 방법 1 */
+  // const number = useSelector((state) => state.counter.number);
+  // const diff = useSelector((state) => state.counter.diff);
+
+  /* useSelector 최적화 방법 2 */
+  // react-redux 의 shallowEqual 함수 사용
+  const { number, diff } = useSelector(
+    (state) => ({
+      number: state.counter.number,
+      diff: state.counter.diff,
+    }),
+    shallowEqual
+  );
 
   // useDispatch 는 리덕스 스토어의 dispatch 를 함수에서 사용 할 수 있게 해주는 Hook
   const dispatch = useDispatch();
