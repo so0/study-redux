21810e1601882b415d4866a209df299fd0db4f38
so0 <syngshm@gmail.com>
2020-10-04 15:49:07 +0900

    App 컴포넌트 리덕스 적용

--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -5,13 +5,15 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
 import rootReducer from './modules';
+import { Provider } from 'react-redux';
 
 const store = createStore(rootReducer); // 스토어 생성
-console.log(store.getState()); // 스토어 상태를 확인
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
