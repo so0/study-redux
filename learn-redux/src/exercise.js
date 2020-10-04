f5dc5b63b3d3ec24eff6f322386faab70ad19817
so0 <syngshm@gmail.com>
2020-10-04 15:24:04 +0900

    리덕스 프로젝트 설정, 리덕스 기본 기능 연습

---
+++ learn-redux/src/exercise.js
@@ -0,0 +1,89 @@
+import { createStore } from 'redux';
+
+// createStore는 스토어를 만들어주는 함수입니다.
+// 리액트 프로젝트에서는 단 하나의 스토어를 만듭니다.
+
+const initialState = {
+  counter: 0,
+  text: '',
+  list: [],
+};
+
+/* 액션 타입 정의 */
+const INCREASE = 'INCREASE';
+const DECREASE = 'DECREASE';
+const CHANGE_TEXT = 'CHANGE_TEXT';
+const ADD_TO_LIST = 'ADD_TO_LIST';
+
+/* 액션 생성함수 정의 */
+function increase() {
+  return {
+    type: INCREASE,
+  };
+}
+
+const decrease = () => ({
+  type: DECREASE,
+});
+
+const changeText = (text) => ({
+  type: CHANGE_TEXT,
+  text,
+});
+
+const addToList = (item) => ({
+  type: ADD_TO_LIST,
+  item,
+});
+
+/* 리듀서 만들기 */
+// 액션 생성함수들을 통해 만들어진 객체들을 참조하여 새로운 상태를 만드는 함수
+// 주의: 리듀서에서는 불변성을 꼭 지켜줘야 합니다!
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case INCREASE:
+      return {
+        ...state,
+        counter: state.counter + 1,
+      };
+    case DECREASE:
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+    case CHANGE_TEXT:
+      return {
+        ...state,
+        text: action.text,
+      };
+    case ADD_TO_LIST:
+      return {
+        ...state,
+        list: state.list.concat(action.item),
+      };
+    default:
+      return state;
+  }
+}
+
+/* 스토어 만들기 */
+const store = createStore(reducer);
+
+console.log(store.getState()); // 현재 store 안에 들어있는 상태를 조회합니다.
+
+// 스토어안에 들어있는 상태가 바뀔 때 마다 호출되는 listener 함수
+const listener = () => {
+  const state = store.getState();
+  console.log('listener');
+  console.log(state);
+};
+
+const unsubscribe = store.subscribe(listener);
+// 구독을 해제하고 싶을 때는 unsubscribe() 를 호출
+
+// 액션들을 디스패치 해봅시다.
+store.dispatch(increase());
+store.dispatch(decrease());
+store.dispatch(changeText('안녕하세요'));
+store.dispatch(addToList({ id: 1, text: '와우' }));
